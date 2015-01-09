4f620bfa857d5c701c477c504818a9e987ced935
Pascutron <sainpasq@gmail.com>
2015-01-09 17:10:08 -0300

    David capo
    yeah

--- Eurotrip/app/src/main/java/com/pascutron/eurotrip/hotel/HotelView.java
+++ Eurotrip/app/src/main/java/com/pascutron/eurotrip/hotel/HotelView.java
@@ -5,9 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
+import com.pascutron.eurotrip.maps.Maps;
+import com.pascutron.eurotrip.model.Hotel;
 import com.pascutron.eurotrip.utils.Constants;
 
 import pascutron.eurotrip.R;
@@ -41,23 +41,42 @@ public class HotelView extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_hotel, container, false);
-        TextView hotel = (TextView) rootView.findViewById(R.id.hotel);
+
+        Hotel h = new Hotel();
 
         switch (getArguments().getInt(COUNTRY)){
 
             case Constants.MADRID:
-                hotel.setText(getResources().getString(R.string.hotel_madrid));
+                h.setAddress(getResources().getString(R.string.address_madrid));
+                h.setName(getResources().getString(R.string.hotel_madrid));
+                h.setLatitud(40.4169646);
+                h.setLongitud(-3.7073943);
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.hotel_main, Maps.newInstance(h)).commit();
                 break;
             case Constants.BARCELONA:
-                hotel.setText(getResources().getString(R.string.hotel_barce));
+                h.setAddress(getResources().getString(R.string.address_barce));
+                h.setName(getResources().getString(R.string.hotel_barce));
+                h.setLatitud(41.4216805);
+                h.setLongitud(2.1805097);
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.hotel_main, Maps.newInstance(h)).commit();
                 break;
             case Constants.PARIS:
-                hotel.setText(getResources().getString(R.string.hotel_paris));
+                h.setAddress(getResources().getString(R.string.address_paris));
+                h.setName(getResources().getString(R.string.hotel_paris));
+                h.setLatitud(48.8224009);
+                h.setLongitud(2.2955953);
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.hotel_main, Maps.newInstance(h)).commit();
                 break;
             case Constants.LONDON:
-                hotel.setText(getResources().getString(R.string.hotel_london));
+                h.setAddress(getResources().getString(R.string.address_london));
+                h.setName(getResources().getString(R.string.hotel_london));
+                h.setLatitud(51.5144756);
+                h.setLongitud(-0.1635618);
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.hotel_main, Maps.newInstance(h)).commit();
                 break;
         }
+
+
 
         return rootView;
     }
--- Eurotrip/app/src/main/java/com/pascutron/eurotrip/maps/Maps.java
+++ Eurotrip/app/src/main/java/com/pascutron/eurotrip/maps/Maps.java
@@ -1,23 +1,35 @@
 package com.pascutron.eurotrip.maps;
 
-import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.pascutron.eurotrip.model.Hotel;
+
+import pascutron.eurotrip.R;
 
 /**
  * Created by spascual on 07/01/2015.
  */
-public class Maps extends MapFragment {
+public class Maps extends Fragment {
 
     private static final String LOC = "loc";
 
-    private Location location;
+    private Hotel hotel;
+    private MapView mapView;
+    private GoogleMap map;
 
-    public static Maps newInstance(Location l){
+    public static Maps newInstance(Hotel l){
 
         Maps fragment = new Maps();
         Bundle b = fragment.getArguments();
@@ -32,11 +44,53 @@ public class Maps extends MapFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        location = getArguments().getParcelable(LOC);
-
-
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapsInitializer.initialize(getActivity());
     }
-}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_maps, container, false);
+        mapView = (MapView) v.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        map = mapView.getMap();
+
+        hotel = getArguments().getParcelable(LOC);
+
+        LatLng mapCenter = new LatLng(hotel.getLatitud(), hotel.getLongitud());
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, 13));
+
+        CameraPosition cameraPosition = CameraPosition.builder().target(mapCenter).zoom(13).bearing(90).build();
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 2000, null);
+
+        map.addMarker(new MarkerOptions().position(mapCenter).title(hotel.getName().toUpperCase())
+                .snippet(hotel.getAddress())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pushpin)));
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+   }
+
